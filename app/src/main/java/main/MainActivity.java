276bfa1675c99bc56234c19219b1795f67888e40
NicolaiF <nico-fre@hotmail.com>
2017-03-30 22:39:07 +0200

    Added back button functionality.

--- app/src/main/java/main/MainActivity.java
+++ app/src/main/java/main/MainActivity.java
@@ -1,19 +1,31 @@
 package main;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import sheep.game.Game;
 
 public class MainActivity extends AppCompatActivity {
+    private Game game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
-        Game game = new Game(this, null);
+        game = new Game(this, null);
         game.pushState(new GameState());
 
         setContentView(game);
     }
+
+    @Override
+    public void onBackPressed() {
+        game.popState();
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
 
 }
