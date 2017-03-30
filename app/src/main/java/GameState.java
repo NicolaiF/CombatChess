f2c0c53f5a62360363ef243d3203cd8febf7d718
NicolaiF <nico-fre@hotmail.com>
2017-03-30 21:07:37 +0200

    Sheep Documentation and basic android classes added

---
+++ app/src/main/java/GameState.java
@@ -0,0 +1,12 @@
+import sheep.game.State;
+import sheep.game.World;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.view.MotionEvent;
+import sheep.input.TouchListener;
+
+public class GameState extends State {
+
+
+}
---
+++ app/src/main/java/MainActivity.java
@@ -0,0 +1,16 @@
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import sheep.game.Game;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        Game game = new Game(this, null);
+        game.pushState(new GameState());
+
+        setContentView(game);
+    }
+}
