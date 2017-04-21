d0ef2005aebfceb823ccdfed7a1061b017c9903a
Jonnern <johannes_ervum@hotmail.com>
2017-04-21 12:04:31 +0200

    Lagt til Constants

---
+++ app/src/main/java/view/Constants.java
@@ -0,0 +1,9 @@
+package view;
+
+public class Constants {
+    public static int SCREEN_WIDTH;
+    public static int SCREEN_HEIGHT;
+    public static float PIECE_WIDTH;
+    public static float PIECE_HEIGHT;
+    public static float SCALE_PIECES;
+}
